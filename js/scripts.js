5ee8d56a2c966f30adfd57747a658dbaafdd5e44
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-07-07 16:42:41 -0700

    add more js conditionals to better toggle between three selectors that are used in the conditiondals. Changes for this were made in the jScript file

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
     var langInput = $("#lang").val();
     var experienceInput = $("#experience").val();
     $(".learn").text(learnInput);
+    $(".lang").text(langInput);
     document.getElementById('name').innerHTML = "Hello " + nameInput + "! Checkout the tracks we chose for you!";
     //conditional statement just for the users experience
     if (experienceInput === 'Yes'){
@@ -17,32 +18,75 @@ $(document).ready(function() {
       $("#experienceNo").show();
       $("#experienceYes").hide();
     }
+
+    // if (langInput === 'Java'){
+    //   $("#cSharp").show();
+    //   $("#css").show();
+    //   $("#ruby").show();
+    //   $("#php").hide();
+    //   $("#java").hide();
+    // }
     //conditional statements for the learn and future selections
-    if(learnInput === 'Front-End Programming' && futureInput === 'Business Owner'){
-      $("#cSharp").show();
-      $("#css").show();
-      $("#ruby").show();
-      $("#php").hide();
-      $("#java").hide();
-    }
-    else if(learnInput === 'Front-End Programming' && futureInput === 'Developer Programmer'){
-      $("#cSharp").hide();
-      $("#css").show();
-      $("#ruby").show();
-      $("#php").hide();
-      $("#java").show();
-    }
-    else if (learnInput === 'Back-End Programming' && futureInput === 'Business Owner') {
+    if(learnInput === 'Front-End Programming' && futureInput === 'Business Owner' && langInput === 'Java'){
       $("#cSharp").show();
       $("#css").show();
       $("#ruby").hide();
       $("#php").hide();
       $("#java").show();
     }
-    else if (learnInput === 'Back-End Programming' && futureInput === 'Developer Programmer') {
+    else if(learnInput === 'Front-End Programming' && futureInput === 'Business Owner' && langInput === 'CSS'){
+      $("#cSharp").show();
+      $("#css").show();
+      $("#ruby").show();
+      $("#php").hide();
+      $("#java").hide();
+    }
+    else if(learnInput === 'Front-End Programming' && futureInput === 'Business Owner' && langInput === 'PHP'){
+      $("#cSharp").show();
+      $("#css").show();
+      $("#ruby").hide();
+      $("#php").show();
+      $("#java").hide();
+    }
+    else if(learnInput === 'Front-End Programming' && futureInput === 'Developer Programmer' && (langInput === 'Java'|| langInput === 'CSS')){
+      $("#cSharp").hide();
+      $("#css").show();
+      $("#ruby").show();
+      $("#php").hide();
+      $("#java").show();
+    }
+    else if(learnInput === 'Front-End Programming' && futureInput === 'Developer Programmer' && langInput === 'PHP'){
+      $("#cSharp").hide();
+      $("#css").show();
+      $("#ruby").show();
+      $("#php").show();
+      $("#java").hide();
+    }
+    else if (learnInput === 'Back-End Programming' && futureInput === 'Business Owner' && (langInput === 'CSS' || langInput === 'Java')) {
+      $("#cSharp").show();
+      $("#css").show();
+      $("#ruby").hide();
+      $("#php").hide();
+      $("#java").show();
+    }
+    else if (learnInput === 'Back-End Programming' && futureInput === 'Business Owner' && langInput === 'PHP') {
+      $("#cSharp").show();
+      $("#css").hide();
+      $("#ruby").hide();
+      $("#php").show();
+      $("#java").show();
+    }
+    else if (learnInput === 'Back-End Programming' && futureInput === 'Developer Programmer' && (langInput === 'PHP' || langInput === 'Java')) {
       $("#cSharp").hide();
       $("#css").hide();
       $("#ruby").show();
+      $("#php").show();
+      $("#java").show();
+    }
+    else if (learnInput === 'Back-End Programming' && futureInput === 'Developer Programmer' && langInput === 'CSS') {
+      $("#cSharp").hide();
+      $("#css").show();
+      $("#ruby").hide();
       $("#php").show();
       $("#java").show();
     }
