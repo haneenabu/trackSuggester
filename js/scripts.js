5b5151aa99fa390df958b0dd33a78a3a91e89cba
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-07-07 11:59:37 -0700

    add js if conditional proof of concept

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  var startInput = $("#start").val();
+  $("#formTrack").submit(function(event) {
+    var startInput = $("#start").val();
+    var learnInput = $("#learn").val();
+    var futureInput =$("input:radio[name=future]:checked").val();
+    var langInput = $("#lang").val();
+    var experienceInput = $("#experience").val();
+
+    $(".learn").text(learnInput);
+    if(learnInput === 'Front-End Programming'){
+      $("#cSharp").show();
+    }
+    event.preventDefault();
+  });
 });
