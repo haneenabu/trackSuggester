117fad1bd766d2396a53874207b4b67b762bcd9b
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-07-07 15:56:50 -0700

    add functionality to ask for users name while using it to address the user by name when they submit their selections

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
+  var nameInput = prompt ("Please enter your name: ");
+
   $("#formTrack").submit(function(event) {
     var startInput = $("#start").val();
     var learnInput = $("#learn").val();
     var futureInput =$("input:radio[name=future]:checked").val();
     var langInput = $("#lang").val();
     var experienceInput = $("#experience").val();
-
     $(".learn").text(learnInput);
+    document.getElementById('name').innerHTML = "Hello " + nameInput + "! Checkout the tracks we chose for you!";
+    //conditional statement just for the users experience
     if (experienceInput === 'Yes'){
       $("#experienceYes").show();
       $("#experienceNo").hide();
@@ -14,7 +17,7 @@ $(document).ready(function() {
       $("#experienceNo").show();
       $("#experienceYes").hide();
     }
-
+    //conditional statements for the learn and future selections
     if(learnInput === 'Front-End Programming' && futureInput === 'Business Owner'){
       $("#cSharp").show();
       $("#css").show();
@@ -45,9 +48,12 @@ $(document).ready(function() {
     }
     else {
       $("#error").show();
+      $("#cSharp").hide();
+      $("#css").hide();
+      $("#ruby").hide();
+      $("#php").hide();
+      $("#java").hide();
     }
-
-
     event.preventDefault();
   });
 });
